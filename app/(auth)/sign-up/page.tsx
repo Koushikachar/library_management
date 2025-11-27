081ecb1e3c9987a40eb2706ef5d279e71a430c39
Koushikachar <Koushikachar25@gmail.com>
2025-11-27 15:04:14 +0530

    first commit

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+import AuthForm from "@/components/AuthForm";
+import { signup } from "@/lib/actions/auth";
+import { signUpSchema } from "@/lib/validation";
+
+const page = () => (
+  <AuthForm
+    type="SIGN_UP"
+    schema={signUpSchema}
+    defaultValues={{
+      email: "",
+      password: "",
+      fullName: "",
+      universityId: 0,
+      universityCard: "",
+    }}
+    onSubmit={signup}
+  />
+);
+
+export default page;
